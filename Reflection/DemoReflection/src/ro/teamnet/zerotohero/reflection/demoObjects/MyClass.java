2e94162d68592e1d144c1f7763f5eb09ac06bd0c
mihait <mihait@teamnet.ro>
2015-04-27 15:15:21 +0300

    Reflection

---
+++ Reflection/DemoReflection/src/ro/teamnet/zerotohero/reflection/demoObjects/MyClass.java
@@ -0,0 +1,11 @@
+package ro.teamnet.zerotohero.reflection.demoObjects;
+
+/** User: mihait Date: 25.10.2014 Time: 01:00 */
+public class MyClass extends SuperClass {
+
+    class InnerClass{
+    }
+
+    A a;
+    B b;
+}
