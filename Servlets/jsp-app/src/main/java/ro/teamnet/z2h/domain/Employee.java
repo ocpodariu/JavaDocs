a5cd89a2ce840df2df119c414fd1a1821b0ccb75
OanaD <danielaoana.besliu@gmail.com>
2015-05-03 12:02:08 +0300

    add JEE projects.

---
+++ Servlets/jsp-app/src/main/java/ro/teamnet/z2h/domain/Employee.java
@@ -0,0 +1,79 @@
+package ro.teamnet.z2h.domain;
+
+import java.util.Date;
+
+public class Employee {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Date hireDate;
+    private Double salary;
+    private Double commissionPoints;
+    private String phoneNumber;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(Date hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Double getCommissionPoints() {
+        return commissionPoints;
+    }
+
+    public void setCommissionPoints(Double commissionPoints) {
+        this.commissionPoints = commissionPoints;
+    }
+}
