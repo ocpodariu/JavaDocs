a1fa891976324a8f77438521b9887049a5988bb4
Vlad Bulimac <bulivlad>
2015-05-05 10:04:01 +0300

    Added Http Session workshop

---
+++ Servlets/httpsession-app/src/main/java/ro/teamnet/zth/web/HelloWorldServlet.java
@@ -0,0 +1,36 @@
+/**
+ * HelloWorldServlet.java
+ *
+ * Copyright (c) 2014 Teamnet. All Rights Reserved.
+ *
+ * This source file may not be copied, modified or redistributed,
+ * in whole or in part, in any form or for any reason, without the express
+ * written consent of Teamnet.
+ **/
+
+package ro.teamnet.zth.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Hello World Servlet expose get method to say hello
+ * to input user
+ */
+public class HelloWorldServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                resp.setContentType("text/html");
+        resp.getWriter().write("Hello <b>"+req.getParameter("user")+"</b>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+
+
+}
